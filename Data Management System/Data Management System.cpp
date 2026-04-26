158e3ca2d4b3924acf279b0508154348a320f52b
Rv2003 <Rusiruvinvidu2017@gmail.com>
2026-04-26 23:21:04 +0530

    basic idea

--- Data Management System/Data Management System.cpp
+++ Data Management System/Data Management System.cpp
@@ -4,9 +4,16 @@
 #include "Header.h"
 using namespace std;
 
+void pre_allocated() { //used to create currently running process 
+	singly_linked_list list;
 
+	list.allocate_memory(1024,"A");
+
+};
 int main()
 {
-    
+	
+
+	
 }
 
