f033fd7321f48a2d744b3603fdb53a72d977cbc9
Rv2003 <Rusiruvinvidu2017@gmail.com>
2026-04-27 21:38:14 +0530

    Basic structure

--- Data Management System/Header.h
+++ Data Management System/Header.h
@@ -11,8 +11,8 @@ public:
     node() {
 
         process = "FREE";
-        next = NULL;   //constructor
-        memory = 0;
+        next = NULL;   //default constructor
+        memory =0;
 
     }
 
@@ -25,36 +25,76 @@ public:
 };
 
 class singly_linked_list {
-   
+
 private:
     node* head;
     node* tail;
     int storage;
-
-public:
-    singly_linked_list() {  //input the size of the memory block
     
+public:
+    int max = 16384;
+    singly_linked_list() {  //input the size of the memory block
+
         head = NULL;
         tail = NULL;
         storage = 0;
-        
+
     }
 
-void allocate_memory(int mem,string k) {
-    node* temp;
-    temp = new node(k, mem);
-        if (head == NULL) {
-            head = tail = temp;
-            
+    void allocate_memory(int mem, string k) {
+        node* temp;
+       
+        if (max > mem) {
+            temp = new node(k, mem);
+
+            if (head == NULL) {
+                head = tail = temp;
+
+            }
+            else {
+                tail->next = temp;
+                tail = temp;
+
+            }
+            max= max - mem;
+               
         }
         else {
-            tail->next = temp;
-            tail = temp;
-            head->memory = head->memory - mem;
+            cout << "FULL";
         }
-        
-}
 
-};}
+    }
+
+    void display() {
+
+        if (max > 0) {
+            node* freeBlock = new node("FREE", max);  // ONE free block at the end
+            tail->next = freeBlock;
+            tail = freeBlock;
+        }
+        if (head == NULL) {
+            cout << "Memory is empty." << endl;
+            return;
+        }
+
+        node* curr = head;
+        int address = 0;
+
+        cout << "------------------------------------------" << endl;
+        cout << "| Start Addr | Process | Size (KB)      |" << endl;
+        cout << "------------------------------------------" << endl;
+
+        while (curr != NULL) {
+            cout << "| " << address
+                << "\t\t| " << curr->process
+                << "\t  | " << curr->memory << " KB"
+                << "\t\t|" << endl;
+            address += curr->memory;
+            curr = curr->next;
+        }
+
+        cout << "------------------------------------------" << endl;
+        cout << "Total Used: " << storage << " KB" << endl;
+    }
 
 };
