d64ca3b9ad8e9289b6672a406fa51bce1abd25d1
Rv2003 <Rusiruvinvidu2017@gmail.com>
2026-04-27 21:37:36 +0530

    Basic structure of the project

--- Data Management System/Data Management System.cpp
+++ Data Management System/Data Management System.cpp
@@ -1,12 +1,18 @@
-
-
 #include <iostream>
 #include "Header.h"
 using namespace std;
 
+void pre_allocated(int A,string B) { //used to create currently running process 
+	singly_linked_list list;
+
+	list.allocate_memory(1024,"A");
+	list.allocate_memory(2048, "FREE");
+	list.allocate_memory(3072, "B");
 
 int main()
 {
-    
+	
+
+	
 }
 
--- Data Management System/Header.h
+++ Data Management System/Header.h
@@ -1,2 +1,60 @@
 #pragma once
 using namespace std;
+
+
+class node  {  
+public:
+    string process; // to store the process name
+    int memory;
+    node* next;
+
+    node() {
+
+        process = "FREE";
+        next = NULL;   //constructor
+        memory = 0;
+
+    }
+
+    node(string e,int size) {
+
+        process = e;
+        next = NULL;   //overloaded constructor 
+        memory = size;
+    }
+};
+
+class singly_linked_list {
+   
+private:
+    node* head;
+    node* tail;
+    int storage;
+
+public:
+    singly_linked_list() {  //input the size of the memory block
+    
+        head = NULL;
+        tail = NULL;
+        storage = 0;
+        
+    }
+
+void allocate_memory(int mem,string k) {
+    node* temp;
+    temp = new node(k, mem);
+        if (head == NULL) {
+            head = tail = temp;
+            
+        }
+        else {
+            tail->next = temp;
+            tail = temp;
+            head->memory = head->memory - mem;
+        }
+        
+}
+
+};}
+
+};
