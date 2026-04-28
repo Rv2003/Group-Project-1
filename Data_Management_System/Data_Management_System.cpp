b1ed0f3bbf15ee5686bb694a0adbadb6f40440e3
Yasiru Nawodya <Yasiru2nawodya@gmail.com>
2026-04-28 23:13:14 +0530

    Renamed files to remove spaces

---
+++ Data_Management_System/Data_Management_System.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+
+void pre_allocated() { //used to create currently running process (currently on ram)
+	singly_linked_list list;
+	
+	list.allocate_memory(1024, "A");
+	list.allocate_memory(2048, "FREE");  
+	list.allocate_memory(3072, "B");
+	list.allocate_memory(2048, "FREE");
+	list.allocate_memory(3072, "C");
+	list.display();
+}
+
+
+
+int main()
+{   
+	
+	pre_allocated();
+
+
+	
+}
+
